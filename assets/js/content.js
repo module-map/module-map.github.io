906f7fd35460224c1a31c2975a237ec0e0bc4fad
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2023-11-30 19:58:22 +0000

    Iterate async

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,6 +1,10 @@
 "use strict";
 const startYear = document.getElementById("startYear");
 const degree = document.getElementById("degree");
+const maths = document.getElementById("maths");
+const chem = document.getElementById("chem");
+
+var modules = [];
 
 function yearFile(year, plus = 0) {
   const y = parseInt(year) + plus;
@@ -8,22 +12,87 @@ function yearFile(year, plus = 0) {
 }
 
 function updateChoices() {
-  console.log("Whooo");
+  console.log("Updating choices");
+  [1, 2, 3].forEach(level => {
+    let credits = 0;
+    let michCredits = 0;
+    let epipCredits = 0;
+    for (const code in modules) {
+      if (modules.hasOwnProperty(code)) {
+        const mod = modules[code];
+        if (mod.level == level && mod.selected) {
+          console.log(mod);
+          const modCreds = mod.credits;
+          credits += mod.credits;
+          if (mod.mich && mod.epip) {
+            michCredits += mod.credits / 2;
+            epipCredits += mod.credits / 2;
+          } else if (mod.mich) {
+            michCredits += mod.credits;
+          } else if (mod.epip) {
+            epipCredits += mod.credits;
+          } else {
+            console.error("Module seems not to run in either term: ", i);
+          }
+        }
+      }
+    }
+    console.log(level);
+    const note = document.getElementById("note" + level);
+    $(note).empty();
+    const credNote = document.createElement("p");
+    const credTot = document.createElement("span");
+    credTot.innerHTML = credits + "/120";
+    if (credits != 120) {
+      credTot.classList.add("invalid");
+    }
+    const michTot = document.createElement("span");
+    const epipTot = document.createElement("span");
+    michTot.innerHTML = michCredits;
+    epipTot.innerHTML = epipCredits;
+    if (michCredits > 70 || michCredits < 50) {
+      michTot.classList.add("invalid");
+    }
+    if (epipCredits > 70 || epipCredits < 50) {
+      epipTot.classList.add("invalid");
+    }
+
+    $(credNote).append(credTot, "&nbsp;credits (", michTot,
+                       " Michaelmas; ", epipTot, " Epiphany)");
+    note.appendChild(credNote);
+  })
 }
 
-function updateStartYear() {
-  [1, 2, 3, 4].forEach(level => {
-    $("#level" + level + " > div").remove();
-    fetch(yearFile(startYear.value, level - 1))
+async function updateParams() {
+  for (const level of [1, 2, 3, 4]) {
+    $("#level" + level + " > .module-box").remove();
+    await fetch(yearFile(startYear.value, level - 1))
       .then(response => response.json())
       .then(data => {
         data.forEach(module => {
           if (module.Level == level) {
-            const required = (module[degree.value] === undefined ?
+            let required = (module[degree.value] === undefined ?
               false : module[degree.value].toUpperCase());
-            console.log(module["Module code"] + " - " + required);
+            // Maths / further maths
+            if (module["Module code"] == "GEOL1061" && required) {
+              required = maths.checked ? "O" : "X";
+            } else if (module["Module code"] == "GEOL1081" && required) {
+              required = maths.checked ? "X" : "O";
+            }
+            modules[module["Module code"]] = {
+              available: required != "O",
+              required: required,
+              selected: required == "X",
+              credits: module.Credits,
+              level: module.Level,
+              mich: module.Mich,
+              epip: module.Epip,
+              req: module.Requisites
+            }
+
             const box = document.createElement("div");
             box.classList.add("module-box");
+
             if (module.Credits == 40) {
               box.classList.add("double");
             } else if (module.Credits == 60) {
@@ -37,24 +106,26 @@ function updateStartYear() {
             }
             if (required == "X") {
               box.classList.add("required");
+              box.setAttribute("chosen", true)
             } else if (required == "O") {
               box.classList.add("unavailable");
             }
 
             const check = document.createElement("input");
             check.type = "checkbox";
-            check.id = module["Module code"];
+            check.id = "check" + module["Module code"];
             if (required == "X") {
               check.checked = "checked";
               check.disabled = "disabled";
             }
-
 
             box.appendChild(check);
             box.addEventListener("click", function(e) {
               if (e.srcElement != check && !check.disabled) {
                 check.checked = !check.checked;
               }
+              modules[module["Module code"]].selected = check.checked;
+              updateChoices();
             })
 
             const text = document.createElement("div");
@@ -72,21 +143,24 @@ function updateStartYear() {
           }
         })
       });
-  });
+  };
+  updateChoices();
 }
 
 fetch("data/years.json")
   .then(response => response.json())
   .then(data => {
-
     data.slice(0, -2).forEach(year => {
       const option = document.createElement("option");
       option.textContent = year.replace("-", "/");
       option.value = year.substring(0, 4);
       startYear.appendChild(option);
     })
-    updateStartYear();
+    updateParams();
   })
   .catch(error => console.error("Error fetching years.json: ", error))
 
-startYear.addEventListener("change", updateStartYear)
+startYear.addEventListener("change", updateParams)
+degree.addEventListener("change", updateParams)
+maths.addEventListener("change", updateParams)
+chem.addEventListener("change", updateParams)
