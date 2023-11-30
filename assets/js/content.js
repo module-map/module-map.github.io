87784f8ac24c85ee2843986bf2b61b68203250f3
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2023-11-30 21:14:15 +0000

    Module selection

--- assets/js/content.js
+++ assets/js/content.js
@@ -12,8 +12,7 @@ function yearFile(year, plus = 0) {
 }
 
 function updateChoices() {
-  console.log("Updating choices");
-  [1, 2, 3].forEach(level => {
+  for (const level of [1, 2, 3]) {
     let credits = 0;
     let michCredits = 0;
     let epipCredits = 0;
@@ -21,7 +20,6 @@ function updateChoices() {
       if (modules.hasOwnProperty(code)) {
         const mod = modules[code];
         if (mod.level == level && mod.selected) {
-          console.log(mod);
           const modCreds = mod.credits;
           credits += mod.credits;
           if (mod.mich && mod.epip) {
@@ -37,7 +35,6 @@ function updateChoices() {
         }
       }
     }
-    console.log(level);
     const note = document.getElementById("note" + level);
     $(note).empty();
     const credNote = document.createElement("p");
@@ -60,12 +57,44 @@ function updateChoices() {
     $(credNote).append(credTot, "&nbsp;credits (", michTot,
                        " Michaelmas; ", epipTot, " Epiphany)");
     note.appendChild(credNote);
-  })
+  }
+}
+
+function setTerm(box, term) {
+  box.classList.remove(term == "mich" ? "epip" : "mich");
+  box.classList.add(term);
+}
+
+function choose(box, chosen = true) {
+  const code = $(box).attr("id");
+  modules[code].selected = chosen;
+  document.getElementById("check" + code).checked = chosen;
+  updateChoices();
+}
+
+function makeAvailable(box, avail = true) {
+  if (avail) {
+    box.classList.remove("unavailable");
+  } else {
+    box.classList.add("unavailable");
+    choose(box, false);
+  }
+}
+
+function makeRequired(box, req = true) {
+  if (req) {
+    makeAvailable(box);
+    box.classList.add("required");
+    box.getElementsByTagName("input").disabled = true;
+    choose(box);
+  } else {
+    box.classList.remove("required");
+    box.getElementsByTagName("input").disabled = false;
+  }
 }
 
 async function updateParams() {
   for (const level of [1, 2, 3, 4]) {
-    $("#level" + level + " > .module-box").remove();
     await fetch(yearFile(startYear.value, level - 1))
       .then(response => response.json())
       .then(data => {
@@ -79,67 +108,72 @@ async function updateParams() {
             } else if (module["Module code"] == "GEOL1081" && required) {
               required = maths.checked ? "X" : "O";
             }
+
+            const moduleExists = modules.hasOwnProperty(module["Module code"]);
+            const box = moduleExists ? modules[module["Module code"]].box :
+                        document.createElement("div");
+
+            if (!moduleExists) {
+              box.classList.add("module-box");
+              box.setAttribute("id", module["Module code"]);
+              if (module.Credits == 40) {
+                box.classList.add("double");
+              } else if (module.Credits == 60) {
+                box.classList.add("triple");
+              }
+
+              const check = document.createElement("input");
+              check.type = "checkbox";
+              check.id = "check" + module["Module code"];
+
+              box.appendChild(check);
+              box.addEventListener("click", function(e) {
+                if (e.srcElement != check && !check.disabled) {
+                  check.checked = !check.checked;
+                }
+                choose(box, check.checked);
+                updateChoices();
+              })
+
+              const text = document.createElement("div");
+              const code = document.createElement("span");
+              code.innerHTML = module["Module code"];
+              code.classList.add("module-code");
+              const name = document.createElement("span");
+              name.innerHTML = module["Module name"];
+              name.classList.add("module-name");
+              text.classList.add("module-text");
+              text.appendChild(name);
+              text.appendChild(code);
+              box.appendChild(text);
+
+              document.getElementById("level" + module.Level).appendChild(box);
+            }
+
+
             modules[module["Module code"]] = {
               available: required != "O",
               required: required,
-              selected: required == "X",
               credits: module.Credits,
               level: module.Level,
               mich: module.Mich,
               epip: module.Epip,
-              req: module.Requisites
+              req: module.Requisites,
+              box: box
             }
 
-            const box = document.createElement("div");
-            box.classList.add("module-box");
-
-            if (module.Credits == 40) {
-              box.classList.add("double");
-            } else if (module.Credits == 60) {
-              box.classList.add("triple");
-            } else if (module.Credits == 10) {
+            if (module.Credits == 10) {
               if (module.Epip) {
-                box.classList.add("epip")
+                setTerm(box, "epip")
               } else if (module.Mich) {
-                box.classList.add("mich")
+                setTerm(box, "mich")
               }
             }
             if (required == "X") {
-              box.classList.add("required");
-              box.setAttribute("chosen", true)
+              makeRequired(box)
             } else if (required == "O") {
-              box.classList.add("unavailable");
+              makeAvailable(box, false);
             }
-
-            const check = document.createElement("input");
-            check.type = "checkbox";
-            check.id = "check" + module["Module code"];
-            if (required == "X") {
-              check.checked = "checked";
-              check.disabled = "disabled";
-            }
-
-            box.appendChild(check);
-            box.addEventListener("click", function(e) {
-              if (e.srcElement != check && !check.disabled) {
-                check.checked = !check.checked;
-              }
-              modules[module["Module code"]].selected = check.checked;
-              updateChoices();
-            })
-
-            const text = document.createElement("div");
-            const code = document.createElement("span");
-            code.innerHTML = module["Module code"];
-            code.classList.add("module-code");
-            const name = document.createElement("span");
-            name.innerHTML = module["Module name"];
-            name.classList.add("module-name");
-            text.classList.add("module-text");
-            text.appendChild(name);
-            text.appendChild(code);
-            box.appendChild(text);
-            document.getElementById("level" + module.Level).appendChild(box);
           }
         })
       });
