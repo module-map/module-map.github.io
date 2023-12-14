ac7f7f39c328f8d04b983a4373e9aa261525c1ad
SMITH <1695515+ms609@users.noreply.github.com>
2023-12-14 11:13:59 +0000

    rm navPanel

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,34 +32,4 @@
 			hideDelay: 350
 		});
 
-	// Nav.
-
-		// Title Bar.
-			$(
-				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-				'</div>'
-			)
-				.appendTo($body);
-
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
-
 })(jQuery);
